58c2d4bdf45ccc49c45d6c099017757da91cea82
guichuan_zhong <guichuan_zhong@163.com>
2025-05-19 11:46:33 +0800

    add explanation in line 39 and line 42

--- 前缀和数组/1314.矩阵区域和.cpp
+++ 前缀和数组/1314.矩阵区域和.cpp
@@ -36,8 +36,10 @@ public:
         vector<vector<int>>res(m,vector<int>(n,0));
         for(int i =0; i<m; i++){
             for(int j = 0; j<n; j++){
+                // 左上角的坐标
                 int x1 = max(0,i-k);
                 int y1 = max(0,j-k);
+                // 右下角坐标
                 int x2 = min(i+k,m-1);
                 int y2 = min(j+k,n-1);
                 res[i][j] = numMatrix.SumRegion(x1,y1,x2,y2);
